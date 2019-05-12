3b240a0d5e1c0d6dc3b3b7738895f1eba52fb28e
Albert I <kras@raphielgang.org>
2019-05-12 20:28:47 +0800

    santoni: Hack some props to allow flashing ROMs with build date check
    * Some ROMs check for ro.build.date.utc, which is false-positive since
      TWRP's build date might be newer than the ROM that we're going to flash.
    * Also, just for kicks and giggles, override ro.bootimage.build.date.utc
      so we can have matching yet consistent POSIX build time.
    
    Change-Id: Id3a19c4a977e8084453c01897e82f193ff8d976d
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Pranaya Deomani <deomanipranaya007@gmail.com>

--- Android.mk
+++ Android.mk
@@ -20,4 +20,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),santoni)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# Hack some props to allow flashing ROMs with build date check
+BOARD_RECOVERY_IMAGE_PREPARE := \
+	sed -i 's/ro.bootimage.build.date.utc=.*/ro.bootimage.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
+	sed -i 's/ro.build.date.utc=.*/ro.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default
 endif
